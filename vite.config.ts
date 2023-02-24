9109f680738c078243708768abe3d0eee1e74f48
glassonion1 <taisuke.fujita@layerx.co.jp>
2023-02-24 23:55:22 +0900

    fix build settings

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,14 @@ import { peerDependencies } from './package.json'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: resolve(__dirname, 'src', 'index.ts'),
       formats: ['es', 'cjs'],
       fileName: (ext) => `index.${ext}.js`
     },
