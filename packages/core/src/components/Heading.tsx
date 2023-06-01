06238d330defb0d7f757054cad4bb8b2e05af460
taisuke.fujita <glassonion999@gmail.com>
2023-06-01 17:31:58 +0900

    Design system 1.2.2

--- packages/core/src/components/Heading.tsx
+++ packages/core/src/components/Heading.tsx
@@ -3,21 +3,18 @@ import { cx } from '../utils/class'
 
 export interface HeadingProps extends React.ComponentProps<'h1'> {}
 
-const common = `
-    font-bold
-`
-
 export const H1 = (props: HeadingProps) => {
   const { className, children, ...restProps } = props
 
   const style = `
-    text-h1
+    text-lm
+    sm:text-l
     mt-16
     mb-6
   `
 
   return (
-    <h1 className={cx(common, style, className)} {...restProps}>
+    <h1 className={cx(style, className)} {...restProps}>
       {children}
     </h1>
   )
@@ -28,13 +25,14 @@ export const H2 = (props: HeadingProps) => {
   const { className, children, ...restProps } = props
 
   const style = `
-    text-h2
+    text-mm
+    sm:text-m
     mt-16
     mb-6
   `
 
   return (
-    <h2 className={cx(common, style, className)} {...restProps}>
+    <h2 className={cx(style, className)} {...restProps}>
       {children}
     </h2>
   )
@@ -45,13 +43,14 @@ export const H3 = (props: HeadingProps) => {
   const { className, children, ...restProps } = props
 
   const style = `
-    text-h3
+    text-sm
+    sm:text-s
     mt-10
     mb-6
   `
 
   return (
-    <h3 className={cx(common, style, className)} {...restProps}>
+    <h3 className={cx(style, className)} {...restProps}>
       {children}
     </h3>
   )
@@ -62,13 +61,14 @@ export const H4 = (props: HeadingProps) => {
   const { className, children, ...restProps } = props
 
   const style = `
-    text-h4
+    text-xsm
+    sm:text-xs
     mt-10
     mb-4
   `
 
   return (
-    <h4 className={cx(common, style, className)} {...restProps}>
+    <h4 className={cx(style, className)} {...restProps}>
       {children}
     </h4>
   )
@@ -79,13 +79,14 @@ export const H5 = (props: HeadingProps) => {
   const { className, children, ...restProps } = props
 
   const style = `
-    text-h5
+    text-xxsm
+    sm:text-xxs
     mt-10
     mb-4
   `
 
   return (
-    <h5 className={cx(common, style, className)} {...restProps}>
+    <h5 className={cx(style, className)} {...restProps}>
       {children}
     </h5>
   )
@@ -96,13 +97,14 @@ export const H6 = (props: HeadingProps) => {
   const { className, children, ...restProps } = props
 
   const style = `
-    text-h6
+    text-xxsm
+    sm:text-xxs
     mt-6
     mb-4
   `
 
   return (
-    <h6 className={cx(common, style, className)} {...restProps}>
+    <h6 className={cx(style, className)} {...restProps}>
       {children}
     </h6>
   )
