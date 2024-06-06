4a4349a665c21c8298d9c3e200226852aee81917
taisuke.fujita <glassonion999@gmail.com>
2024-06-06 17:54:18 +0900

    remove unused code

--- packages/core/src/components/Link.tsx
+++ packages/core/src/components/Link.tsx
@@ -23,7 +23,6 @@ export const Link: ComponentWithAs<'a', LinkProps> = forwardRef(
 
     // Make lines break at each word
     const style = `
-      rounded-sm
       cursor-pointer
       text-blue-1000
       active:text-orange-700
@@ -40,16 +39,6 @@ export const Link: ComponentWithAs<'a', LinkProps> = forwardRef(
       align-middle
       font-icon
       font-light
-      antialiased
-    `
-
-    const styleIcon2 = `
-      align-middle
-      ml-0.5
-      text-sumi-700
-      font-icon
-      font-light
-      leading-4
       antialiased
     `
 
