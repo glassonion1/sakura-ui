8112e95e031266fd157657a3eb7e49db1ec54b4f
glassonion1 <glassonion999@gmail.com>
2025-08-03 13:40:09 +0900

    chore: upgrade tailwind

--- packages/forms/vite.config.ts
+++ packages/forms/vite.config.ts
@@ -3,10 +3,11 @@ import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { peerDependencies } from './package.json'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
