86bea512ae08dbe0efd967bf7736d960506baf97
glassonion1 <glassonion999@gmail.com>
2025-08-03 16:47:44 +0900

    update

--- examples/tailwindcss.config.ts
+++ examples/tailwindcss.config.ts
@@ -4,10 +4,9 @@ import sakuraPlugin from '@sakura-ui/tailwind-theme-plugin'
 const config: Config = {
   //  mode: 'jit',
   content: [
-    './index.html',
-    './src/**/*.{jsx,tsx}',
     './node_modules/@sakura-ui/core/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@sakura-ui/forms/**/*.{js,ts,jsx,tsx}'
+    './node_modules/@sakura-ui/forms/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@sakura-ui/helper/**/*.{js,ts,jsx,tsx}'
   ],
   plugins: [sakuraPlugin]
 }
